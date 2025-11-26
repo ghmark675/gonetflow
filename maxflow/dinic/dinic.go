78899ac2750345243e0179d450cb31d9f16a9323
ghmark675 <ghmark675@163.com>
2025-11-26 15:26:31 +0800

    feat: implement Dinic's algorithm for maximum flow calculation

---
+++ maxflow/dinic/dinic.go
@@ -0,0 +1,64 @@
+package maxflow
+
+import (
+	"container/list"
+	"math"
+
+	"github.com/ghmark675/gonetflow/graph"
+)
+
+func buildLevelGraph(gra *graph.Graph, s, t int) bool {
+	for i := range gra.Hig {
+		gra.Hig[i] = -1
+	}
+	que := list.New()
+	gra.Hig[0] = 0
+	que.PushBack(s)
+	for que.Len() > 0 {
+		u, _ := que.Front().Value.(int)
+		que.Remove(que.Front())
+		for i := range gra.Gra[u] {
+			v, c := gra.Edges[i].To, gra.Edges[i].Cap
+			if c > 0 && gra.Hig[v] == -1 {
+				gra.Hig[v] = gra.Hig[u] + 1
+				if v == t {
+					return true
+				}
+				que.PushBack(v)
+			}
+		}
+	}
+	return false
+}
+
+func findAugmentingPath(gra *graph.Graph, u, t, f int) int {
+	if u == t {
+		return f
+	}
+	r := f
+	for ; gra.Cur[u] < len(gra.Gra[u]); gra.Cur[u]++ {
+		j := gra.Gra[u][gra.Cur[u]]
+		ed := &gra.Edges[j]
+		v := ed.To
+		c := ed.Cap
+		if c > 0 && gra.Hig[v] == gra.Hig[u]+1 {
+			a := findAugmentingPath(gra, v, t, min(r, c))
+			gra.Edges[j].Cap -= a
+			gra.Edges[j^1].Cap += a
+			r -= a
+			if r == 0 {
+				return f
+			}
+		}
+	}
+	return f - r
+}
+
+func Dinic(gra *graph.Graph, s, t int) int {
+	ans := 0
+	for buildLevelGraph(gra, s, t) {
+		gra.Cur = make([]int, gra.Nodenum)
+		ans += findAugmentingPath(gra, s, t, math.MaxInt)
+	}
+	return ans
+}
